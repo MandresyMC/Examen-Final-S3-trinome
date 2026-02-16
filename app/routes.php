f1353ee4a89b51157b02038e515471ad0fb733f6
MMC <m.zanajatovo@gmail.com>
2026-02-16 16:08:14 +0300

    debug

--- app/routes.php
+++ app/routes.php
@@ -3,11 +3,11 @@ session_start();
 require_once __DIR__ . '/controllers/DonsController.php';
 require_once __DIR__ . '/controllers/DashboardController.php';
 
-require_once __DIR__ . '/controllers/BesoinRepository.php';
-require_once __DIR__ . '/controllers/DashboardRepository.php';
-require_once __DIR__ . '/controllers/DonsRepository.php';
-require_once __DIR__ . '/controllers/StockDonsRepository.php';
-require_once __DIR__ . '/controllers/VilleRepository.php';
+require_once __DIR__ . '/repositories/BesoinRepository.php';
+require_once __DIR__ . '/repositories/DashboardRepository.php';
+require_once __DIR__ . '/repositories/DonsRepository.php';
+require_once __DIR__ . '/repositories/StockDonsRepository.php';
+require_once __DIR__ . '/repositories/VilleRepository.php';
 
 Flight::route('GET /', function () {
     Flight::redirect('/formulaire_dons');
