c7fcdf7560b780f03601fbf2a6d69b7c50f88e67
MMC <m.zanajatovo@gmail.com>
2026-02-16 21:35:49 +0300

    modification selon la V2 du projet

--- database/schema.sql
+++ database/schema.sql
@@ -11,26 +11,61 @@ CREATE TABLE categorie (
     nom VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE besoin (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    idCategorie INT,
-    idVille INT, -- ville mangataka
-    nomProduit VARCHAR(255) NOT NULL, -- ex : riz, huile, tole
-    quantiteDemandee DOUBLE NOT NULL
-);
+-- CREATE TABLE besoin (
+--     id INT PRIMARY KEY AUTO_INCREMENT,
+--     idCategorie INT,
+--     idVille INT, -- ville mangataka
+--     nomProduit VARCHAR(255) NOT NULL, -- ex : riz, huile, tole
+--     quantiteDemandee DOUBLE NOT NULL
+-- );
 
-CREATE TABLE stockDons (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    idCategorie INT,
-    nomProduit VARCHAR(255) NOT NULL, -- ex : riz, huile, tole
-    quantiteInitiale DOUBLE NOT NULL,
-    quantiteFinale DOUBLE NOT NULL
-);
+-- CREATE TABLE stockDons (
+--     id INT PRIMARY KEY AUTO_INCREMENT,
+--     idCategorie INT,
+--     nomProduit VARCHAR(255) NOT NULL, -- ex : riz, huile, tole
+--     quantiteInitiale DOUBLE NOT NULL,
+--     quantiteFinale DOUBLE NOT NULL
+-- );
 
 CREATE TABLE dons (
     id INT PRIMARY KEY AUTO_INCREMENT,
     idVille INT, -- ville omena
     idStock INT,
     quantiteDonnee DOUBLE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- V1
+
+ALTER TABLE ville ADD COLUMN fond DOUBLE DEFAULT 0;
+
+CREATE TABLE produit (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(255) NOT NULL,
+    idCategorie INT,
+    prixUnitaire DOUBLE NOT NULL
+);
+
+CREATE TABLE besoin (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    idCategorie INT,
+    idVille INT, -- ville mangataka
+    idProduit VARCHAR(255) NOT NULL,
+    quantiteDemandee DOUBLE NOT NULL
+);
+
+CREATE TABLE stockDons (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    idCategorie INT,
+    idProduit VARCHAR(255) NOT NULL, -- ex : riz, huile, tole
+    quantiteInitiale DOUBLE NOT NULL,
+    quantiteFinale DOUBLE NOT NULL
+);
+
+CREATE TABLE achat (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    idVille INT, -- ville manao achat
+    idStock INT, -- stock (produit) hovidiana
+    idDon INT, -- don (produit) hovidiana
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
