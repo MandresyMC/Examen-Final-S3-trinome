59dbe2b59e9f933ae7225e3b7808dddf6eede1a9
Rakotondrazanajatovo Sandratra Mathieu <szanajatovo@gmail.com>
2026-02-17 06:38:28 +0900

    dashboard maj

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -1,11 +1,12 @@
 <?php
     class DashboardController {
 
-        public function showDashboard() {
+        public function showDashboar() {
             $pdo  = Flight::db();
             $repo = new DashboardRepository($pdo);
             
-            $allObjets = $repo->findDashboard();
+            $besoin = $repo->findDashboardBesoin();
+            $achat = $repo->findDashboard();
 
             $success = null;
             if (isset(Flight::request()->query['success'])) {
@@ -13,9 +14,11 @@
             }
 
             Flight::render("dashboard", [
-                'allObjets' => $allObjets,
+                'besoin' => $besoin,
+                'achat' => $besoin,
             ]);
         }
+
     }
 
 ?>
--- app/repositories/DashboardRepository.php
+++ app/repositories/DashboardRepository.php
@@ -4,20 +4,46 @@
         private $pdo;
         public function __construct(PDO $pdo) { $this->pdo = $pdo; }
 
-        public function findDashboard() {
+        public function findDashboardBesoin() {
             $sql = "
-                SELECT b.idCategorie AS idCat,
-                       b.quantiteDemandee AS quantiteDemande,
-                       p.nom AS nomProduit,
-                       v.nom AS villeNom,
-                       d.quantiteDonnee AS quantiteDonne,
-                       a.prix AS achat,
-                       a.created_at AS dateAchat
-                FROM besoin b
-                JOIN produit p ON b.idProduit = p.id
-                JOIN ville v ON b.idVille = v.id
-                JOIN dons d ON b.idVille = d.idVille
-                JOIN achat a ON a.idVille = d.idVille
+                SELECT
+                b.idCategorie AS idCat,
+                v.nom AS villeNom,
+                p.nom AS nomProduit,
+                b.quantiteDemandee AS quantiteDemandee,
+                COALESCE(SUM(d.quantiteDonnee), 0) AS totalDonne
+            FROM besoin b
+            JOIN produit p ON b.idProduit = p.id
+            JOIN ville v ON b.idVille = v.id
+            LEFT JOIN dons d 
+                ON d.idVille = b.idVille
+            GROUP BY b.id, v.nom, p.nom, b.quantiteDemandee, b.idCategorie;
+            ";
+            $st = $this->pdo->prepare($sql);
+            try {
+                $st->execute();
+            } catch (PDOException $e) {
+                $info = $st->errorInfo();
+                throw new RuntimeException('FINDALL : DB error in findAll(): ' . $e->getMessage() . ' - SQLSTATE: ' . ($info[0] ?? '') . ' - DriverMsg: ' . ($info[2] ?? ''));
+            }
+            
+            return $st->fetchAll();
+        }
+
+        public function findDashboardAchat() {
+            $sql = "
+                SELECT
+                    v.nom AS villeNom,
+                    p.nom AS nomProduit,
+                    a.quantite AS quantiteAchetee,
+                    a.prix AS prixAchat,
+                    sd.quantiteInitiale,
+                    sd.quantiteFinale,
+                    a.created_at As dateAchat
+                FROM achat a
+                JOIN ville v ON a.idVille = v.id
+                JOIN stockDons sd ON a.idStock = sd.id
+                JOIN produit p ON sd.idProduit = p.id;
             ";
             $st = $this->pdo->prepare($sql);
             try {
